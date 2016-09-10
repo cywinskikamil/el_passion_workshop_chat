8bdcf7178a9802b1310acf881f73810e5533bc86
cywinskikamil <cywinski.kamil@yahoo.com>
2016-09-10 17:12:50 +0200

    Fix session model

--- web/models/session.ex
+++ web/models/session.ex
@@ -2,11 +2,18 @@ defmodule ElPasssionWorkshopChat.Session do
   alias ElPasssionWorkshopChat.User
   alias ElPasssionWorkshopChat.Repo
 
-  def login(session_params) do
-    user = Repo.get_by!(User, %{email: String.downcase(session_params["email"])})
-    case Comeonin.Bcrypt.checkpw(session_params["password"], user.crypted_password) do
+  def login(params) do
+    user = Repo.get_by(User, email: String.downcase(params["email"]))
+    case authenticate(user, params["password"]) do
       true -> {:ok, user}
-      _ -> :error
+      _    -> :error
+    end
+  end
+
+  defp authenticate(user, password) do
+    case user do
+      nil -> false
+      _   -> Comeonin.Bcrypt.checkpw(password, user.crypted_password)
     end
   end
 end
